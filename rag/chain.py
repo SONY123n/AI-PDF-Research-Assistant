e1db9a56530e75c41f3e5212498a69b531e5e3f5
Sony Jarupula <jarupulasony81@gmail.com>
2026-08-01 11:28:49 +0530

    Fix LLM model

--- rag/chain.py
+++ rag/chain.py
@@ -17,7 +17,7 @@ class RAGChain:
         self.retriever = retriever
 
         self.llm = ChatGoogleGenerativeAI(
-            model="gemini-2.0-flash",
+            model="gemini-1.5-flash",
             google_api_key=os.getenv("GOOGLE_API_KEY"),
             temperature=0.3,
         )
